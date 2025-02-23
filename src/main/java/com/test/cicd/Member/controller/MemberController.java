3f450b06cea84e6e5e968e5904d5b0c88471d6de
woosanghyun0122 <junuary2@naver.com>
2025-02-23 20:44:13 +0900

    18차 커밋

--- src/main/java/com/test/cicd/Member/controller/MemberController.java
+++ src/main/java/com/test/cicd/Member/controller/MemberController.java
@@ -23,6 +23,7 @@ public class MemberController {
         log.info("Controller Start!!");
         List<Member> list = service.findAll();
         model.addAttribute("list", list);
+        log.info("Controller end!!");
         return "member/list";
     }
 
